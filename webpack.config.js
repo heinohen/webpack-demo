1da92115e1cbca61b1c945505c5940ea32d53411
Henrik Heinonen <hohein@utu.fi>
2022-09-20 23:50:49 +0300

    dist -> docs

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,20 @@ const path = require("path");
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'docs')
     },
-    mode: 'development'
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 }
