b8da8d03c233fb9b50cb5afcb1c5f1b559e9d1b7
Henrik Heinonen <hohein@utu.fi>
2022-09-20 23:45:10 +0300

    images added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import _ from 'lodash';
 import myName from './myName';
 import {drinkOne, drinkDislike} from './drinks';
+import './style.css';
+import Icon from './images/megaphone.png';
 
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML= _.join(['Hello', 'webpack'], ' ');
-    element.textContent = myName('TOFI!');
+    element.textContent = myName('Hen6!');
+    const megap = new Image();
+    megap.classList.add("megaphone");
+    megap.src = Icon;
+    element.appendChild(megap);
 
 
     return element;
@@ -24,7 +30,18 @@ function alsoAnComponent() {
 
 }
 
+function helloWorld() {
+    const helloWorld = document.createElement('div');
+
+    helloWorld.textContent = "Hello World!";
+    helloWorld.classList.add('hello');
+
+
+    return helloWorld;
+
+}
+
 document.body.appendChild(component());
 document.body.appendChild(alsoAnComponent());
-
+document.body.appendChild(helloWorld());
 
